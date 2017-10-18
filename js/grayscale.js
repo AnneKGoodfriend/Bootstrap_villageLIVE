10c26320b885ee1affa4a3883fb992c5c7b6081b
AnneKGoodfriend <annekgoodfriend@gmail.com>
2017-10-17 21:43:33 -0400

    switched video for image

--- js/grayscale.js
+++ js/grayscale.js
@@ -35,155 +35,10 @@ $('.navbar-collapse ul li a').click(function() {
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', init360Banner);
 google.maps.event.addDomListener(window, 'resize', function() {
     // map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
 });
-
-function init() {
-    init360Banner();
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var mapOptions = {
-    //     // How zoomed in you want the map to start at (always required)
-    //     zoom: 15,
-
-    //     // The latitude and longitude to center the map (always required)
-    //     center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-    //     // Disables the default Google Maps UI components
-    //     disableDefaultUI: true,
-    //     scrollwheel: false,
-    //     draggable: false,
-
-    //     // How you would like to style the map.
-    //     // This is where you would paste any style found on Snazzy Maps.
-    //     styles: [{
-    //         "featureType": "water",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 17
-    //         }]
-    //     }, {
-    //         "featureType": "landscape",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 20
-    //         }]
-    //     }, {
-    //         "featureType": "road.highway",
-    //         "elementType": "geometry.fill",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 17
-    //         }]
-    //     }, {
-    //         "featureType": "road.highway",
-    //         "elementType": "geometry.stroke",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 29
-    //         }, {
-    //             "weight": 0.2
-    //         }]
-    //     }, {
-    //         "featureType": "road.arterial",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 18
-    //         }]
-    //     }, {
-    //         "featureType": "road.local",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 16
-    //         }]
-    //     }, {
-    //         "featureType": "poi",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 21
-    //         }]
-    //     }, {
-    //         "elementType": "labels.text.stroke",
-    //         "stylers": [{
-    //             "visibility": "on"
-    //         }, {
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 16
-    //         }]
-    //     }, {
-    //         "elementType": "labels.text.fill",
-    //         "stylers": [{
-    //             "saturation": 36
-    //         }, {
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 40
-    //         }]
-    //     }, {
-    //         "elementType": "labels.icon",
-    //         "stylers": [{
-    //             "visibility": "off"
-    //         }]
-    //     }, {
-    //         "featureType": "transit",
-    //         "elementType": "geometry",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 19
-    //         }]
-    //     }, {
-    //         "featureType": "administrative",
-    //         "elementType": "geometry.fill",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 20
-    //         }]
-    //     }, {
-    //         "featureType": "administrative",
-    //         "elementType": "geometry.stroke",
-    //         "stylers": [{
-    //             "color": "#000000"
-    //         }, {
-    //             "lightness": 17
-    //         }, {
-    //             "weight": 1.2
-    //         }]
-    //     }]
-    // };
-
-    // // Get the HTML DOM element that will contain your map
-    // // We are using a div with id="map" seen below in the <body>
-    // var mapElement = document.getElementById('map');
-
-    // // Create the Google Map using out element and options defined above
-    // map = new google.maps.Map(mapElement, mapOptions);
-
-    // // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    // var image = 'img/map-marker.png';
-    // var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    // var beachMarker = new google.maps.Marker({
-    //     position: myLatLng,
-    //     map: map,
-    //     icon: image
-    // });
-}
 
 
 //Three.js 360 video banner setup
@@ -204,30 +59,22 @@ function init360Banner(){
     var geometry = new THREE.SphereGeometry( 500, 60, 40 );
     geometry.scale( - 1, 1, 1 );
     
-    video = document.createElement( 'video' );
-    video.src = 'assets/prototype.mp4';
-    video.type = "video/mp4";
-	video.loop = true;
-	video.muted = true;
-    // video.autoplay = true;
-    video.load();
-    video.addEventListener('loadeddata', function() {
-        // Hide the loader div that covers everything!
+ //    video = document.createElement( 'video' );
+ //    video.src = 'assets/prototype.mp4';
+ //    video.type = "video/mp4";
+	// video.loop = true;
+	// video.muted = true;
+ //    // video.autoplay = true;
+ //    video.load();
+ //    video.addEventListener('loadeddata', function() {
+ //        // Hide the loader div that covers everything!
 
-        // video.play();
-    }, false);
+ //        // video.play();
+ //    }, false);
 
-        // document.getElementById("intro").addEventListener("click", function(e){
-        //     //e.preventDefault();
-        //     video.play();
-        // });
-	
 
-    //imgTex = document.createElement( 'img' );
-    // imgTex.src = '';
-    //Some sort of if statement based on platform
-
-    texture = new THREE.VideoTexture( video );
+ //    texture = new THREE.VideoTexture( video );
+    texture = THREE.ImageUtils.loadTexture('assets/prototype.png');
     texture.minFilter = THREE.NearestFilter;
 	texture.maxFilter = THREE.NearestFilter;
 	texture.format = THREE.RGBFormat;
