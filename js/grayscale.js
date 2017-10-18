660fa2d3bf8b6fe1e2fc9bee38ea9acc96cc0dd2
AnneKGoodfriend <annekgoodfriend@gmail.com>
2017-10-17 21:51:58 -0400

    switched video for image, loading

--- js/grayscale.js
+++ js/grayscale.js
@@ -35,7 +35,7 @@ $('.navbar-collapse ul li a').click(function() {
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
-//google.maps.event.addDomListener(window, 'load', init360Banner);
+google.maps.event.addDomListener(window, 'load', init360Banner);
 //google.maps.event.addDomListener(window, 'resize', function() {
     // map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
 //});
